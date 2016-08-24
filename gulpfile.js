30bc6b956b3daa4a9fb06728e2473a7550e2f00a
Attreid <attreid@gmail.com>
2016-08-24 13:24:10 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,7 @@
 var gulp = require('gulp'),
         less = require('gulp-less'),
-        sass = require('gulp-sass'),
         minify = require('gulp-clean-css'),
-        concat = require('gulp-concat'),
-        uglify = require('gulp-uglify'),
         rename = require('gulp-rename');
-
-var paths = {
-    'dev': {
-        'less': './resources/assets/js/',
-        'vendor': './resources/assets/vendor/'
-    },
-    'production': {
-        'css': './assets/'
-    }
-};
-
-var files = [
-    paths.dev.js + 'MD5Hasher.js',
-    paths.dev.js + 'RemoveDiacritics.js',
-    paths.dev.js + 'jQuery.js',
-    paths.dev.js + 'String.js',
-    paths.dev.js + 'Number.js'
-];
 
 gulp.task('less', function () {
     return gulp.src('./assets/cookiePolicy.less')
