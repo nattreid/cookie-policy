4bb608549b484a86b1dee59adbe39a499218e138
attreid <attreid@gmail.com>
2017-09-12 13:34:55 +0200

    pridany jazyky pl a sk

---
+++ src/Lang/pl.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'cookiePolicy.text' => 'Ważne: Strona wykorzystuje pliki cookies.',
+	'cookiePolicy.link' => 'Więcej informacji',
+	'cookiePolicy.understand' => 'Rozumiem',
+];
---
+++ src/Lang/sk.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'cookiePolicy.text' => 'Tento web používa súbory cookie. Ďalším prechádzaním tohto webu vyjadrujete súhlas s ich používaním.',
+	'cookiePolicy.link' => 'Viac informácií',
+	'cookiePolicy.understand' => 'Rozumiem',
+];
