c1170b372925ef5cc6a9dd7f00053b92c69020a4
attreid <attreid@gmail.com>
2018-06-08 09:15:12 +0200

    lang

--- src/Lang/de.php
+++ src/Lang/de.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
-
+	'nattreid.cookiePolicy.text' => 'Um Inhalte und Werbung für konkrete Nutzer personalisieren, Funktionen von Social Media bereitstellen und die Besucheranzahl unserer Webseite analysieren zu können, nutzen wir Cookie-Dateien. Die Informationen über Ihr Nutzerverhalten auf unserer Webseite teilen wir mit unseren Social-Media- und Werbepartnern und unseren Partnern für die Verarbeitung der Analysedaten. Dies steht alles im Einklang mit dem Dokument zu den Grundsätzen über die Verarbeitung personenbezogener Daten. Im Abschnitt „Cookie-Einstellungen“ können Sie Ihre Präferenzen bearbeiten. Durch die Nutzung dieser Webseite stimmen Sie der Benutzung von Cookie-Dateien zu.',
+	'nattreid.cookiePolicy.link' => 'Mehr Informationen',
+	'nattreid.cookiePolicy.settings' => 'Cookie-Einstellungen',
+	'nattreid.cookiePolicy.privacySettings' => 'Privatsphäre-EinstellungenNastavení soukromí',
+	'nattreid.cookiePolicy.allowAll' => 'Alles erlauben',
+	'nattreid.cookiePolicy.save' => 'Einstellungen speichern',
+	'nattreid.cookiePolicy.allowWithoutCommercial' => 'Ohne zielgerichtete Werbung erlauben',
+	'nattreid.cookiePolicy.title' => 'Zentrale zum Einstellen des Privatsphäreschutzes',
+	'nattreid.cookiePolicy.alwaysActive' => 'Immer aktiviert',
+	'nattreid.cookiePolicy.active' => 'Aktiviert',
+	'nattreid.cookiePolicy.inactive' => 'Deaktiviert',
+	'nattreid.cookiePolicy.privacy.title' => 'Ihre Privatsphäre',
+	'nattreid.cookiePolicy.privacy.content' => 'Die Webseiten, die Sie besuchen, können Informationen aus Ihrem Browsers speichern und auslesen, üblicherweise mithilfe von Cookies. Diese Informationen können Sie selbst, Ihre Präferenzen oder Ihr Gerät betreffen und werden in der Regel zur Optimierung von Webinhalten und deren Funktionen genutzt. Es handelt sich um Informationen, die für gewöhnlich eine direkte Identifizierung Ihrer Person verunmöglichen, können allerdings dazu beitragen, dass sich die Webinhalte Ihren Präferenzen und Interessensgebieten anpassen.<br/><br/>Da wir Recht auf Privatsphäre respektieren, bieten wir Ihnen die Möglichkeit einige Cookie-Arten nicht zu erlauben. Durch Klicken auf die einzelnen Kategorien können Sie sich nähere Informationen durchlesen und die Grundeinstellungen anpassen. Sollten Sie manche Cookie-Arten blockieren, kann dies unter Umständen dazu führen, dass die Webseite nicht optimal funktioniert und einige Dienste nicht zur Verfügung stehen müssen.',
+	'nattreid.cookiePolicy.needed.title' => 'Notwendige Cookies-Dateien',
+	'nattreid.cookiePolicy.needed.content' => 'Diese Dateien sind für das Funktionieren der Webseite unverzichtbar und können in unserem System nicht deaktiviert werden. In der Regel sind sie so eingestellt, dass sie lediglich auf Ihre Aktionen reagieren, das heisst Ihre Nachfrage nach Diensten wie beispielsweise Einstellungen zum Schutz Ihrer Privatsphäre, die Anmeldung oder das Ausfüllen eines Formulars. Ohne diese Cookie-Dateien könnten wir Ihnen einige Bereiche unserer Webseite nicht zugänglich machen oder einige nachgefragte Dienste nicht anbieten.',
+	'nattreid.cookiePolicy.analytics.title' => 'Analytische Cookie-Dateien',
+	'nattreid.cookiePolicy.analytics.content' => 'Diese Dateien ermöglichen es uns, die Besucheranzahl unserer Webseite zu verfolgen und den wie einzelne Benutzer zu uns gelangen, damit wir die Leistung unserer Webseite messen und optimieren können. Dadurch wissen wir auch welche Webseiten am beliebtesten sind und auf welche Weise sich Besucher im Web bewegen.<br/><br/>Sämtliche Informationen, die diese Cookie-Dateien ansammlen, werden zusammengeführt und sind somit anonymisiert. Sollten Sie diese Art Cookies nicht erlauben, erfahren wir nicht, wann Sie unsere Webseite besuchen, und werden die Leistung unserer Webseite nicht messen.',
+	'nattreid.cookiePolicy.functional.title' => 'Funktionale Cookie-Dateien',
+	'nattreid.cookiePolicy.functional.content' => 'Diese Dateien ermöglichen es uns, die Webseite an Sie anzupassen und Ihnen mehr Funktionen anbieten zu können. Sie dienen unter Anderem der Speicherung Ihrer Auswahl, der Sprach- oder der Lageeinstellung. Diese Art Cookies werden von uns oder Dritten eingestellt, deren Dienste wir auf unserer Webseite vorstellen.<br/><br/>Sollten Sie diese Art Cookies nicht erlauben, werden eventuell einige oder keine dieser Dienste richtig funktionieren.',
+	'nattreid.cookiePolicy.commercial.title' => 'Werbe-Cookie-Dateien',
+	'nattreid.cookiePolicy.commercial.content' => 'Die Einstellung dieser Dateien führen wir oder unsere Werbepartner durch. Diese Gesellschaften können mit deren Hilfe Ihr Interessenprofil erstellen und Ihnen so für Sie relevante Werbung auf anderen Webseiten anbieten.<br/><br/>Es werden nicht direkt personenbezogene Daten gespeichert, es wird aber mit einem einzigartigen Identifikationsverfahren des Browsers und des Geräts, mit dem Sie sich mit dem Internet verbinden, gearbeitet. Sollten Sie diese Art Cookies nicht erlauben, wird Ihnen weniger zielgerichtete Werbung angezeigt werden.',
 ];
--- src/Lang/en.php
+++ src/Lang/en.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
-
+	'nattreid.cookiePolicy.text' => 'We use cookie files to adapt the content and advertising, provide social media function and analyze attendance of our web for individual users.  We share information about your work on web with our partners for social media, advertising and processing of analysis in conformity with Principles of processing personal data documents. In a part „Setting of cookie files, you can set your preferences. By using it you agree with cookie files.',
+	'nattreid.cookiePolicy.link' => 'More information',
+	'nattreid.cookiePolicy.settings' => 'Cookie setting',
+	'nattreid.cookiePolicy.privacySettings' => 'Setting of privacy',
+	'nattreid.cookiePolicy.allowAll' => 'Permit all',
+	'nattreid.cookiePolicy.save' => 'Save setting',
+	'nattreid.cookiePolicy.allowWithoutCommercial' => 'Permit without targeted advertising',
+	'nattreid.cookiePolicy.title' => 'Centre of privacy protection preferences',
+	'nattreid.cookiePolicy.alwaysActive' => 'Always active',
+	'nattreid.cookiePolicy.active' => 'Active',
+	'nattreid.cookiePolicy.inactive' => 'Not active',
+	'nattreid.cookiePolicy.privacy.title' => 'Your privacy',
+	'nattreid.cookiePolicy.privacy.content' => 'Webs, which you are visiting, can save or load information from your browser, usually by cookie files. This information could concern you, your preferences or your equipment and it is used for securing of optimal web working. It´s about information, which usually don’t enable direct check of your identity, but could help to adapt web to your requirements and interest.<br/><br/>We respect that you are entitled to your privacy, so we offer you the possibility to ban some cookie files. After the click on individual category you will see detailed information and possibility to adjust the initial setting.  If you ban some types of cookie files, web works optimally and don´t  need to use some services.',
+	'nattreid.cookiePolicy.needed.title' => 'Totally necessary cookie files',
+	'nattreid.cookiePolicy.needed.content' => 'These cookie files are necessary for web working and it´s not possible to deactivate them in our systems. It´s usually set to react according to your action, so according to services requests, e.g. setting of privacy protection preferences, registration or filling of form. We couldn´t make accessible some web parts or provide some required services without these cookie files.',
+	'nattreid.cookiePolicy.analytics.title' => 'Analytic cookie files',
+	'nattreid.cookiePolicy.analytics.content' => 'These cookie files enable us to monitor amount of visits and where the users come from to measure and improve web working. We know thanks to it, which pages are the most favorite, which are less and how the users move on web.<br/><br/>All information, which this cookie files collect are anonymous. If you ban this cookie files during the visiting our web, we couldn´t check, how the web work.',
+	'nattreid.cookiePolicy.functional.title' => 'Function cookie files',
+	'nattreid.cookiePolicy.functional.content' => 'These cookie files enable to adapt web for you and offer you more functions. It´s also for saving your choices, preferred language or destination. Cookie files are set by us or by the third side, which we present on our web.<br/><br/>In case, you don´t confirm this function, some or none of them will not work correctly.',
+	'nattreid.cookiePolicy.commercial.title' => 'Advertising cookie files',
+	'nattreid.cookiePolicy.commercial.content' => 'This cookie files are set by us or our partnership advertisers. These companies can create a profile of your interests and thanks to it they offer you relevant advertising.<br/><br/>They don´t save directly personal data, but work with unique browse identification and equipment, thanks to it you connect to the internet. In case, you ban this cookie files, you will see less targeted advertising.',
 ];
