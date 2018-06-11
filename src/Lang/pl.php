8426a4691cedf2a33be4524d9894eee599cc2756
attreid <attreid@gmail.com>
2018-06-11 12:03:34 +0200

    lang

--- src/Lang/pl.php
+++ src/Lang/pl.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
-
+	'nattreid.cookiePolicy.text' => 'Aby móc dostosować treść oraz reklamy według konkretnych użytkowników, aby móc świadczyć funkcje medii społecznościowych oraz analizować w jaki sposób ludzie odwiedzają naszą strone, używamy plików cookies. Infromacje o Twojej pracy z internetem przekazujemy naszym partnerom do spraw mediów społecznościowych, ogłoszeń publicznych i analizy danych a to zgodnie z dokumentem Zasady przetwarzania danych osobowych W części „Ustawienie plików cookie“ możesz modyfikować swoje preferecje. Korzystaniem z niniejszej strony internetowej zgadzasz się na używanie plików cookies.',
+	'nattreid.cookiePolicy.link' => 'Więcej informacji',
+	'nattreid.cookiePolicy.settings' => 'Ustawienia plików Cookies',
+	'nattreid.cookiePolicy.privacySettings' => 'Ustawienie prywatności',
+	'nattreid.cookiePolicy.allowAll' => 'Pozwalaj na wszystko',
+	'nattreid.cookiePolicy.save' => 'Zapisz ustawienia',
+	'nattreid.cookiePolicy.allowWithoutCommercial' => 'Pozwalaj bez reklamy spersonalizowanej',
+	'nattreid.cookiePolicy.title' => 'Centrum preferencji ochrony prywatności',
+	'nattreid.cookiePolicy.alwaysActive' => 'Zawsze aktywne',
+	'nattreid.cookiePolicy.active' => 'Aktywne',
+	'nattreid.cookiePolicy.inactive' => 'Nieaktywne',
+	'nattreid.cookiePolicy.privacy.title' => 'Twoja prywatność',
+	'nattreid.cookiePolicy.privacy.content' => 'Strony internetowe, które odwiedzasz, mogą zapisywać lub ładować informacje z twojej przeglądarki, zazwyczaj w postaci plików cookies Informacje te mogą dotyczyć Ciebie, Twoich preferencji lub Twojego urządzenia i zazwyczaj stosuje się ich do zapewnienia prawidłowego działania internetu. Chodzi o informacje, które zazwyczaj nie pozwalają na bezpośrednie ujawnienie Twojej tożsamości, lecz mogą służyć do tego, aby się internet dostosował do Twoich wymagań oraz zainteresowań.<br/><br/>Ponieważ szanujemy Twoje prawo na prywatność, oferujemy możliwość zakazać niektóre typy plików cookies. Po kliknięciu na pojedyncze katerogie możesz przeczytać bardziej szczegółowe informacje oraz modyfikować ustawienia domyślne. W przypadku zablokowania niektórych typów plików cookies nasza strona internetowa nie musi działać optymalnie oraz nie muszą być dostępnie niektóre jej funkcjonalności',
+	'nattreid.cookiePolicy.needed.title' => 'Niezbędne pliki cookies',
+	'nattreid.cookiePolicy.needed.content' => 'Niniejsze pliki cookies są niezbędne dla prawidłowego działania naszej strony i w naszych systemach nie ma możliwości ich wyłączenia. Zazwyczaj są ustawione w taki sposób, aby reagowały tylko na Twoje polecenia, tzn. na żądania o usługi, np. na ustawienie preferencji ochrony prywatności, zalogowanie się lub wypełnienie formularza. Bez tych plików cookies nie bylibyśmy w stanie udostępnić niektórych części strony internetowej lub świadczyć niektórych żądanych usług.',
+	'nattreid.cookiePolicy.analytics.title' => 'Analityczne pliki cookies',
+	'nattreid.cookiePolicy.analytics.content' => 'Niniejsze pliki cookies pozwalają nam śledzić ilość wyświetleń naszej strony oraz skąd na naszą strone użytkownicy przychodzą a to w celu mierzenia oraz poprawiania efektywności strony internetowej. Dzięki temu wiemy, które strony są najbardziej ulubione a które nie oraz w jaki sposób poruszają się użytkownicy po naszej stronie. <br/><br/>Wszelkie informacje zebrane w ten sposób są zapisywane w jednym miejscu, przez co jest zapewniona ich anonimowość. Jeśli zablokujesz tych plików cookies, nie będziemy mogli widzieć kiedy odwiedzasz naszą stronę, ani jaką ma strona efektywność.',
+	'nattreid.cookiePolicy.functional.title' => 'Funkcyjne pliki cookies',
+	'nattreid.cookiePolicy.functional.content' => 'Niniejesze pliki cookies umożliwiają lepsze dostosowanie strony internetowej do Twoich wymagań oraz umożliwiają zaoferowanie więcej funkcji. Poza tym zapisują Twoje preferencje co do języka strony lub lokalizacji. Pliki cookies tego rodzaju ustawiamy my, lub strony trzecie, których usługi są prezentowane na naszej stronie.<br/><br/>Jeśli zablokujesz niniejsze pliki cookies, niektóre lub wszystko usługi tego typu nie muszą działać prawidłowo.',
+	'nattreid.cookiePolicy.commercial.title' => 'Reklamowe pliki cookies',
+	'nattreid.cookiePolicy.commercial.content' => 'Pliki te ustawiamy my, lub nasi partnerzy udostępniający swoje oferty na naszej stronie. Niniejsze firmy mogą za pomocą tych plików stworzyć profił Twoich zainteresowań i na podstawie tego oferować spersonalizowaną reklamę na swoich stronachch.<br/><br/>Nie zapisują danyc osobowych, lecz pracują z unikalnym identyfikatorem przeglądarki oraz urządzenia z którego korzystasz. Jeśli zablokujesz niniejsze pliki będą Ci się wyświetlać mniej spersonalizowane reklamy.',
 ];
