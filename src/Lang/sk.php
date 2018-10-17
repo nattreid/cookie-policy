b1c1024b9d0ee117d5a1f111801789b3bf4ada78
attreid <attreid@gmail.com>
2018-10-17 09:06:48 +0200

    lang update

--- src/Lang/sk.php
+++ src/Lang/sk.php
@@ -1,25 +1,39 @@
 <?php
 
 return [
-	'nattreid.cookiePolicy.text' => 'Aby sme mohli prispôsobiť obsah a reklamy konkrétnym užívateľom, poskytovať funkcie sociálnych médií a analyzovať návštevnosť nášho webu, používame súbory cookie. Informácie o vašej práci s webom tiež zdieľame s našimi partnermi pre sociálne média, inzerciu a spracovanie analýzy, a to v súlade s dokumentom Zásady spracovania osobných údajov. V časti „Nastavenie súborov cookie“ môžete upraviť svoje preferencie. Používaním tohoto webu vyjadrujete súhlas s používaním súborov cookie.',
-	'nattreid.cookiePolicy.link' => 'Viac informácií',
-	'nattreid.cookiePolicy.settings' => 'Cookie nastavenie',
-	'nattreid.cookiePolicy.privacySettings' => 'Ochrana súkromia a nastavenia',
-	'nattreid.cookiePolicy.allowAll' => 'Povoliť všetko',
-	'nattreid.cookiePolicy.save' => 'Uložiť nastavenia',
-	'nattreid.cookiePolicy.allowWithoutCommercial' => 'Povoliť bez cielenej reklamy',
-	'nattreid.cookiePolicy.title' => 'Centrum preferencií ochrany súkromia',
-	'nattreid.cookiePolicy.alwaysActive' => 'Vždy aktívny',
-	'nattreid.cookiePolicy.active' => 'Aktívny',
-	'nattreid.cookiePolicy.inactive' => 'Neaktívny',
-	'nattreid.cookiePolicy.privacy.title' => 'Vaše súkromie',
-	'nattreid.cookiePolicy.privacy.content' => 'Weby, ktoré navštevujete, mohou ukládat nebo načítat informácie z vášho prehliadača, zvyčajne formou súborov cookie. Tieto informácie sa môžu týkať vás, vašich preferencií alebo vášho zariadenia a spravidla sa používajú na zabezpečenie optimálneho fungovania webu. Ide o informácie, ktoré zvyčajne neumožňujú priame potvrdenie vašej totožnosti, ale môžu napomôcť k tomu, aby sa web prispôsobil vašim požiadavkam a záujmom.<br/><br/>Pretože rešpektujeme vaše právo na súkromie a ponúkame vám možnosť niektoré typy soúborov cookie zakázať. Po kliknutí na jednotlivé kategórie si môžete prečítať podrobnejšie informácie a upraviť predvolené nastavenia. V prípade zablokovania niektorých typov súborov cookie web nemusí fungovať optimálne a nemusí byť k dispozícii nniektoré služby.',
-	'nattreid.cookiePolicy.needed.title' => 'Naprosto nezbytné soubory cookie',
-	'nattreid.cookiePolicy.needed.content' => 'Tieto súbory cookie sú nevyhnutné pre fungovanie webu a v našich systémoch ich nie je možné deaktivovať. Zvyčajne sú nastavené tak, aby reagovali iba na vaše akcie, teda na žiadosti o služby, napríklad na nastavenie preferencií ochrany súkromia, prihlásenia alebo vyplnenie formulára. Bez týchto súborov cookie by sme vám nemohli sprístupniť niektoré časti webu alebo poskytnúť niektoré požadované služby.',
-	'nattreid.cookiePolicy.analytics.title' => 'Analytické súbory cookie',
-	'nattreid.cookiePolicy.analytics.content' => 'Tieto súbory cookie nám umožňujú sledovať počet návštevy a odkiaľ na náš web užívatelia prichádzajú, aby sme mohli merať a zlepšovať výkon webu. Vďaka tomuto vieme, ktoré stránky sú nejobľúbenejšie a najmenej obľúbené a akým spôsobom sa návštevníci na webe pohybujú.<br/><br/>Všetky informácie, ktoré tieto súbory cookie zhromažďujú, sa agregujú, a tým pádom sú anonymné. Ak tieto súbory cookie zakážete, nedozvieme sa, kedy náš web navštevujete, a nebudeme môcť sledovať, ako si web vedie.',
-	'nattreid.cookiePolicy.functional.title' => 'Funkčné súbory cookie',
-	'nattreid.cookiePolicy.functional.content' => 'Tieto súbory cookie umožňujú, aby sa vám web lepšie prispôsobil a aby sme vám mohli ponúknuť viac funkcií. Okrem iného slúži na uloženie vašich volieb, uprednostňovaného jazyka alebo polohy. Súbory cookie tohto typu nastavujeme my alebo tretie strany, ktorých služby na svojich stránkach prezentujeme.<br/><br/>Pokiaľ tieto súbory cookie nepovolíte, niektoré alebo žiadne z týchto služieb nemusia fungovať správne.',
-	'nattreid.cookiePolicy.commercial.title' => 'Reklamné súbory cookie',
-	'nattreid.cookiePolicy.commercial.content' => 'Tieto súbory cookie nastavujeme my alebo naši partnerský inzerenti. Tieto spoločnosti si s ich pomocou môžu vytvoriť profil vašich záujmov a na základe toho vám na iných stránkách môžu ponúkať relevantné reklamy.<br/><br/>Neukladajú priamo osobné údaje, ale pracujú s jedinečnou identifikáciou prehliadača a zariadenia, prostredníctvom ktorého sa pripájate k internetu. Pokiaľ tieto súbory cookie zakážete, budú sa vám zobrazovať menej cielené reklamy.',
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Aby sme mohli prispôsobiť obsah a reklamy konkrétnym užívateľom, poskytovať funkcie sociálnych médií a analyzovať návštevnosť nášho webu, používame súbory cookie. Informácie o vašej práci s webom tiež zdieľame s našimi partnermi pre sociálne média, inzerciu a spracovanie analýzy, a to v súlade s dokumentom Zásady spracovania osobných údajov. V časti „Nastavenie súborov cookie“ môžete upraviť svoje preferencie. Používaním tohoto webu vyjadrujete súhlas s používaním súborov cookie.',
+			'link' => 'Viac informácií',
+			'settings' => 'Cookie nastavenie',
+			'privacySettings' => 'Ochrana súkromia a nastavenia',
+			'allowAll' => 'Povoliť všetko',
+			'save' => 'Uložiť nastavenia',
+			'allowWithoutCommercial' => 'Povoliť bez cielenej reklamy',
+			'title' => 'Centrum preferencií ochrany súkromia',
+			'alwaysActive' => 'Vždy aktívny',
+			'active' => 'Aktívny',
+			'inactive' => 'Neaktívny',
+			'privacy' => [
+				'title' => 'Vaše súkromie',
+				'content' => 'Weby, ktoré navštevujete, mohou ukládat nebo načítat informácie z vášho prehliadača, zvyčajne formou súborov cookie. Tieto informácie sa môžu týkať vás, vašich preferencií alebo vášho zariadenia a spravidla sa používajú na zabezpečenie optimálneho fungovania webu. Ide o informácie, ktoré zvyčajne neumožňujú priame potvrdenie vašej totožnosti, ale môžu napomôcť k tomu, aby sa web prispôsobil vašim požiadavkam a záujmom.<br/><br/>Pretože rešpektujeme vaše právo na súkromie a ponúkame vám možnosť niektoré typy soúborov cookie zakázať. Po kliknutí na jednotlivé kategórie si môžete prečítať podrobnejšie informácie a upraviť predvolené nastavenia. V prípade zablokovania niektorých typov súborov cookie web nemusí fungovať optimálne a nemusí byť k dispozícii nniektoré služby.',
+			],
+			'needed' => [
+				'title' => 'Naprosto nezbytné soubory cookie',
+				'content' => 'Tieto súbory cookie sú nevyhnutné pre fungovanie webu a v našich systémoch ich nie je možné deaktivovať. Zvyčajne sú nastavené tak, aby reagovali iba na vaše akcie, teda na žiadosti o služby, napríklad na nastavenie preferencií ochrany súkromia, prihlásenia alebo vyplnenie formulára. Bez týchto súborov cookie by sme vám nemohli sprístupniť niektoré časti webu alebo poskytnúť niektoré požadované služby.',
+			],
+			'analytics' => [
+				'title' => 'Analytické súbory cookie',
+				'content' => 'Tieto súbory cookie nám umožňujú sledovať počet návštevy a odkiaľ na náš web užívatelia prichádzajú, aby sme mohli merať a zlepšovať výkon webu. Vďaka tomuto vieme, ktoré stránky sú nejobľúbenejšie a najmenej obľúbené a akým spôsobom sa návštevníci na webe pohybujú.<br/><br/>Všetky informácie, ktoré tieto súbory cookie zhromažďujú, sa agregujú, a tým pádom sú anonymné. Ak tieto súbory cookie zakážete, nedozvieme sa, kedy náš web navštevujete, a nebudeme môcť sledovať, ako si web vedie.',
+			],
+			'functional' => [
+				'title' => 'Funkčné súbory cookie',
+				'content' => 'Tieto súbory cookie umožňujú, aby sa vám web lepšie prispôsobil a aby sme vám mohli ponúknuť viac funkcií. Okrem iného slúži na uloženie vašich volieb, uprednostňovaného jazyka alebo polohy. Súbory cookie tohto typu nastavujeme my alebo tretie strany, ktorých služby na svojich stránkach prezentujeme.<br/><br/>Pokiaľ tieto súbory cookie nepovolíte, niektoré alebo žiadne z týchto služieb nemusia fungovať správne.',
+			],
+			'commercial' => [
+				'title' => 'Reklamné súbory cookie',
+				'content' => 'Tieto súbory cookie nastavujeme my alebo naši partnerský inzerenti. Tieto spoločnosti si s ich pomocou môžu vytvoriť profil vašich záujmov a na základe toho vám na iných stránkách môžu ponúkať relevantné reklamy.<br/><br/>Neukladajú priamo osobné údaje, ale pracujú s jedinečnou identifikáciou prehliadača a zariadenia, prostredníctvom ktorého sa pripájate k internetu. Pokiaľ tieto súbory cookie zakážete, budú sa vám zobrazovať menej cielené reklamy.',
+			]
+		]
+	]
 ];
