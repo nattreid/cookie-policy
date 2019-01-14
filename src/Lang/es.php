fc9f213fbe8b26d93ae4ab0f92bcc9d8104e668a
attreid <attreid@gmail.com>
2019-01-14 09:28:41 +0100

    pridana spanelstina

---
+++ src/Lang/es.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Para que podamos ajustar el contenido de la página y los anuncios apropiar a los usarios concretos, otorgar las funciónes de medios sociales y analizar el nùmero de visitantes usamos los archivos cookies.   La información sobre su uso de la página web compartemos con nuestros compa?eros para medios sociales, publicidades y procesamiento de análisis en consecuencia con el documento Principios del procesamiento de los datos personales. En la parte “El ajuste de los archivos cookie“ puede ajustar su preferencias. Por usar esta página web está de acuerdo con el uso de los archivos cookies.',
+			'link' => 'Más información',
+			'settings' => 'Ajuste de Cookies',
+			'privacySettings' => 'Protección de la privacidad y el ajuste',
+			'allowAll' => 'Permitir todo',
+			'save' => 'Guardar el ajustamiento',
+			'allowWithoutCommercial' => 'Permitir sin el anuncio apropiado',
+			'title' => 'Centro de de las preferencias de la protección de la privacidad',
+			'alwaysActive' => 'Siempre activo',
+			'active' => 'Activo',
+			'inactive' => 'No activo',
+			'privacy' => [
+				'title' => 'Su privacidad',
+				'content' => 'Las páginas web que visita pueden guardar o descargar informaciónes de su navegador normalmente por archivos cookies. Esta información se pueden relacionar con su persona, sus preferencias o su aparato y generalmente se usan para garantizar el funcionamiento correcto de la página web. Se trata de información que normalmente no permiten descubrir su persona directamente, sin embargo pueden ayudar la página web que se adapte según sus requisitos e interéses. <br/><br/>Porque respetamos su derecho de la privacidad, le ofrecemos la posiblidad de prohibir algunos tipos de cookies. Para leer más información, haga clic en la categoría concreta, también puede manejar el ajuste. En el caso de bloquear algunos tipos de archivos cookie la página web puede funcionar incorrectamente.',
+			],
+			'needed' => [
+				'title' => 'Archivos cookies imprescindibles',
+				'content' => 'Estos archivos cookies son imprescindibles para el funcionamiento de nuestra página web y no los podemos desactivar. Normalmente son ajustados de manera que reaccionen solo a su actividad, es decir, a los requisitos  de los servicios, por ejemplo, el ajuste de las preferencias de la protección de la privacidad, rellenar un formulario o la inscripción. Sin dichos archivos cookies no pudieramos hacer disponible algunas partes de nuestra página web u ofrecer servicios pedidos.',
+			],
+			'analytics' => [
+				'title' => 'Archivos cookies de análisis',
+				'content' => 'Estos archivos nos permiten número de los visitantes y de donde los visitante vienen a nuestra web para que podamos medir y perfeccionar la efectividad. Gracías a esto sabemos que páginas son más favoritas y de que modo los visitantes usan nuestra página. <br/><br/>Toda la información que está acumulada por dichos archivos, está agregada, es decir, anónima. Si decide prohibir los archivos cookies, no vamos a enterarnos, cuando visita nuestra página y de que modo la usa.',
+			],
+			'functional' => [
+				'title' => 'Archivos cookies de funcionamiento',
+				'content' => 'Estos archivos cookies permiten que se la página web apropie y le podamos ofrecer más funciónes. Entre otros, sirven para guardar nuestras opciónes, idioma elegida o la locación. Los archivos cookies de este tipo ajustamos o lo hace el tercero cuyos servicios presentamos en nuestra web. <br/><br/>Si no permite los archivos cookies, algunos de aquellos servicios pueden funcionar de manera incorrecta.',
+			],
+			'commercial' => [
+				'title' => 'Archivos cookies del anuncio',
+				'content' => 'Estos archivos cookies ajustamos nosotros o nuestros compa?eros. Aquellas compa?ías pueden crear perfil de interéses suyos y basandose en dicho perfil pueden ofrecer anuncio relevante en ptras páginas.<br/><br/>No guardan concretamente los datos personales, sino trabajan con idetificación única de navegador y aparato mediante que ha conectado a la red. Si prohibe dichos archivos cookies, le van a presentar cantidad más peque?a de los anuncios apropiados.',
+			]
+		]
+	]
+];
