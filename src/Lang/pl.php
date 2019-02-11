4b59225ced9ed6bbb85672060712c07a42be1c6b
attreid <attreid@gmail.com>
2019-02-11 08:45:29 +0100

    uprava pl

--- src/Lang/pl.php
+++ src/Lang/pl.php
@@ -1,9 +1,9 @@
-<?php
+﻿<?php
 
 return [
 	'nattreid' => [
 		'cookiePolicy' => [
-			'text' => 'Aby móc dostosować treść oraz reklamy według konkretnych użytkowników, aby móc świadczyć funkcje medii społecznościowych oraz analizować w jaki sposób ludzie odwiedzają naszą strone, używamy plików cookies. Infromacje o Twojej pracy z internetem przekazujemy naszym partnerom do spraw mediów społecznościowych, ogłoszeń publicznych i analizy danych a to zgodnie z dokumentem Zasady przetwarzania danych osobowych W części „Ustawienie plików cookie“ możesz modyfikować swoje preferecje. Korzystaniem z niniejszej strony internetowej zgadzasz się na używanie plików cookies.',
+			'text' => 'Aby móc dostosować treść oraz reklamy do konkretnych użytkowników, aby móc świadczyć funkcje mediów społecznościowych oraz analizować w jaki sposób ludzie odwiedzają naszą stronę, używamy plików cookies. Informacje o Twojej pracy z internetem przekazujemy naszym partnerom do spraw mediów społecznościowych, ogłoszeń publicznych i analizy danych, a to zgodnie z dokumentem Zasady przetwarzania danych osobowych. W części „Ustawienie plików cookie“ możesz modyfikować swoje preferencje. Korzystaniem z niniejszej strony internetowej zgadzasz się na używanie plików cookies.',
 			'link' => 'Więcej informacji',
 			'settings' => 'Ustawienia plików Cookies',
 			'privacySettings' => 'Ustawienie prywatności',
@@ -16,7 +16,7 @@ return [
 			'inactive' => 'Nieaktywne',
 			'privacy' => [
 				'title' => 'Twoja prywatność',
-				'content' => 'Strony internetowe, które odwiedzasz, mogą zapisywać lub ładować informacje z twojej przeglądarki, zazwyczaj w postaci plików cookies Informacje te mogą dotyczyć Ciebie, Twoich preferencji lub Twojego urządzenia i zazwyczaj stosuje się ich do zapewnienia prawidłowego działania internetu. Chodzi o informacje, które zazwyczaj nie pozwalają na bezpośrednie ujawnienie Twojej tożsamości, lecz mogą służyć do tego, aby się internet dostosował do Twoich wymagań oraz zainteresowań.<br/><br/>Ponieważ szanujemy Twoje prawo na prywatność, oferujemy możliwość zakazać niektóre typy plików cookies. Po kliknięciu na pojedyncze katerogie możesz przeczytać bardziej szczegółowe informacje oraz modyfikować ustawienia domyślne. W przypadku zablokowania niektórych typów plików cookies nasza strona internetowa nie musi działać optymalnie oraz nie muszą być dostępnie niektóre jej funkcjonalności',
+				'content' => 'Strony internetowe, które odwiedzasz, mogą zapisywać lub ładować informacje z Twojej przeglądarki, zazwyczaj w postaci plików cookies. Informacje te mogą dotyczyć Ciebie, Twoich preferencji lub Twojego urządzenia i zazwyczaj stosuje się ich do zapewnienia prawidłowego działania internetu. Chodzi o informacje, które zazwyczaj nie pozwalają na bezpośrednie ujawnienie Twojej tożsamości, lecz mogą służyć do tego, aby się internet dostosował do Twoich wymagań oraz zainteresowań.<br/><br/>Ponieważ szanujemy Twoje prawo na prywatność, oferujemy możliwość zakazać niektóre typy plików cookies. Po kliknięciu na pojedyncze kategorie możesz przeczytać bardziej szczegółowe informacje oraz modyfikować ustawienia domyślne. W przypadku zablokowania niektórych typów plików cookies nasza strona internetowa nie może udostępniać wszystkich funkcji oraz działać optymalnie',
 			],
 			'needed' => [
 				'title' => 'Niezbędne pliki cookies',
@@ -24,15 +24,15 @@ return [
 			],
 			'analytics' => [
 				'title' => 'Analityczne pliki cookies',
-				'content' => 'Niniejsze pliki cookies pozwalają nam śledzić ilość wyświetleń naszej strony oraz skąd na naszą strone użytkownicy przychodzą a to w celu mierzenia oraz poprawiania efektywności strony internetowej. Dzięki temu wiemy, które strony są najbardziej ulubione a które nie oraz w jaki sposób poruszają się użytkownicy po naszej stronie. <br/><br/>Wszelkie informacje zebrane w ten sposób są zapisywane w jednym miejscu, przez co jest zapewniona ich anonimowość. Jeśli zablokujesz tych plików cookies, nie będziemy mogli widzieć kiedy odwiedzasz naszą stronę, ani jaką ma strona efektywność.',
+				'content' => 'Niniejsze pliki cookies pozwalają nam śledzić ilość wyświetleń naszej strony oraz skąd na naszą stronę użytkownicy przychodzą, a to w celu mierzenia oraz poprawiania efektywności strony internetowej. Dzięki temu wiemy, które strony są najbardziej ulubione, a które nie oraz w jaki sposób poruszają się użytkownicy po naszej stronie. <br/><br/>Wszelkie informacje zebrane w ten sposób są zapisywane w jednym miejscu, przez co jest zapewniona ich anonimowość. Po zablokowaniu plików cookies, nie otrzymamy informacji, kiedy odwiedzasz naszą stronę oraz jaką ma strona efektywność.',
 			],
 			'functional' => [
 				'title' => 'Funkcyjne pliki cookies',
-				'content' => 'Niniejesze pliki cookies umożliwiają lepsze dostosowanie strony internetowej do Twoich wymagań oraz umożliwiają zaoferowanie więcej funkcji. Poza tym zapisują Twoje preferencje co do języka strony lub lokalizacji. Pliki cookies tego rodzaju ustawiamy my, lub strony trzecie, których usługi są prezentowane na naszej stronie.<br/><br/>Jeśli zablokujesz niniejsze pliki cookies, niektóre lub wszystko usługi tego typu nie muszą działać prawidłowo.',
+				'content' => 'Niniejsze pliki cookies umożliwiają lepsze dostosowanie strony internetowej do Twoich wymagań oraz zaoferowanie większej funkcjonalności strony. Poza tym zapisują Twoje preferencje dotyczące języka strony lub lokalizacji. Pliki cookies tego rodzaju ustawiamy my, lub strony trzecie, których usługi są prezentowane na naszej stronie.<br/><br/>Jeśli zablokujesz niniejsze pliki cookies, niektóre usługi tego typu nie muszą działać prawidłowo.',
 			],
 			'commercial' => [
 				'title' => 'Reklamowe pliki cookies',
-				'content' => 'Pliki te ustawiamy my, lub nasi partnerzy udostępniający swoje oferty na naszej stronie. Niniejsze firmy mogą za pomocą tych plików stworzyć profił Twoich zainteresowań i na podstawie tego oferować spersonalizowaną reklamę na swoich stronachch.<br/><br/>Nie zapisują danyc osobowych, lecz pracują z unikalnym identyfikatorem przeglądarki oraz urządzenia z którego korzystasz. Jeśli zablokujesz niniejsze pliki będą Ci się wyświetlać mniej spersonalizowane reklamy.',
+				'content' => 'Pliki te ustawiamy my, lub nasi partnerzy udostępniający swoje oferty na naszej stronie. Niniejsze firmy mogą za pomocą tych plików stworzyć profil Twoich zainteresowań i na podstawie tego oferować spersonalizowaną reklamę na swoich stronach.<br/><br/>Nie zapisują danych osobowych, lecz pracują z unikalnym identyfikatorem przeglądarki oraz urządzenia z którego korzystasz. Jeśli zablokujesz niniejsze pliki, będą Ci się wyświetlać mniej spersonalizowane reklamy.',
 			]
 		]
 	]
