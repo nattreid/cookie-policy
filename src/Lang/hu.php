aecf8e2ba98c7fbda507215bda84352d25f692e1
attreid <attreid@gmail.com>
2019-02-13 09:09:27 +0100

    hu lang

---
+++ src/Lang/hu.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'A tartalom és a hirdetések egyéni felhasználók számára történő testreszabására, a szociális média funkciók biztosítása és a webhelyünk forgalomának elemzése érdekében cookie-kat használunk. Mi is megosztjuk a weboldalunkon folytatott munkájával kapcsolatos információkat a szociális médiával, hirdetésekkel és elemzésekkel foglalkozó partnereinkkel, mindezt az Adatvédelmi Szabályzatnak megfelelően tesszük. A cookie-k felhasználásának megváltoztatása lehetséges a böngésző beállításaiban a „Beállítások“ menüpont alatt. A webhely használatával elfogadja a cookie-k használatát.',
+			'link' => 'További információk',
+			'settings' => 'Cookie beállítások',
+			'privacySettings' => 'Adatvédelmi beállítások',
+			'allowAll' => 'Megengedem',
+			'save' => 'Beállítások mentése',
+			'allowWithoutCommercial' => 'Megengedés célzott reklám nélkül',
+			'title' => 'Adatvédelmi beállítások központja',
+			'alwaysActive' => 'Mindig aktív',
+			'active' => 'Aktív',
+			'inactive' => 'Nem aktív',
+			'privacy' => [
+				'title' => 'Az ön adatvédelme',
+				'content' => 'Az ön által meglátogatott webhelyek információkat tárolhatnak vagy kérhetnek le a böngészőből, ez általában a cookie fájlok formájában történik. Ezek az információk Önre, a beállításokra vagy a készülékre vonatkoznak, és általában a web megfelelő teljesítményű  müködése biztosítására használják.  Olyan információkról van szó, amelyek általában nem teszik lehetővé az Ön közvetlenül azonosítását, de segíthetnek a web személyre szabásában az Ön igényeinek érdekeinek.<br/><br/> Tiszteletben tartjuk az Önre vonatkozó adatvédelmet, és felajánljuk Önnek a cookie-k bizonyos típusainak letiltását. Az egyes kategóriákra való kattintás után részletesebb információkat olvashat, és szerkesztheti az alapértelmezett beállításokat. Előfordulhat, hogy egyes cookie-k blokkolása után a web nem működik megfelelően, és egyes szolgáltatások nem érhetők el.',
+			],
+			'needed' => [
+				'title' => 'Teljesen szükséges cookie-k',
+				'content' => 'Ezek a cookie-k szükségesek a webhely működéséhez, és a rendszerünkben nem deaktiválhatók. Jellemzően úgy vannak beállítva, hogy csak az Ön tevékenységeire válaszoljanak, azaz a szolgáltatáskérésekre, például az adatvédelmi beállítások beállítására, bejelentkezésre vagy az űrlap kitöltésére.  Ezen cookie-k nélkül előfordulhat, hogy nem tudják elérni a webes felület egyes részeit, vagy bizonyos szolgáltatásokat.',
+			],
+			'analytics' => [
+				'title' => 'Analitikus cookie fájlok',
+				'content' => 'Ezek a cookie-k lehetővé teszik számunkra, hogy nyomon tudjuk követni a látogatások számát, és azt, hogy  webes felhasználóink honnan jönnek az oldalunkra. Így mérhetjük, és javíthatjuk a webes felületünk teljesítőképességét. Ennek köszönhetően tudjuk, hogy mely webhelyek a legnépszerűbbek és a legkevésbé népszerűek, és azt hogy egyes  látogatók hogyan mozognak a weben. <br/> <br/>Minden olyan információ, amely ezeket a cookie-kat összegyűjti aggregálódik, és ezért névtelen. Ha letiltja ezeket a cookie-kat, akkor nem fogjuk tudni, hogy mikor látogat el a webhelyünkre, és továbbá azt sem, hogy a webhelyünk hogyan működik.',
+			],
+			'functional' => [
+				'title' => 'Működési cookie fájlok',
+				'content' => 'Ezek a cookie-k lehetővé teszik, hogy jobban testreszabhatja webhelyét, és további funkciókat kínálhassunk Önnek. Többek között mentheti a választásait, a preferált nyelvet vagy a helyzeteket. Az ilyen típusú cookie-kat mi vagy a harmadik fél állítja be, akinek a szolgáltatásai az oldalunkon bemutatásra kerülnek.<br/><br/> Ha nem engedélyezi ezeket a cookie-kat, ezen szolgáltatások közül néhány vagy egyik sem fog megfelelőden működni.',
+			],
+			'commercial' => [
+				'title' => 'Reklám cookie fájlok',
+				'content' => 'Ezeket a cookie-k mi általunk vagy a partner hirdetőink által kerülnek beállításra. Ezek a vállalatok létrehozhatnak egy profilt az Ön érdeklődéséről, és ez alapján segíthetnek Önnek megtalálni a releváns hirdetéseket más webhelyeken. <br/> <br/> Nem tárolnak személyes adatokat közvetlenül, de a böngésző és a bejelentkezési eszköz egyedi azonosítójával működnek, amelyen keresztül csatlakozik az internethez. Ha letiltja ezeket  a cookie típusokat,  akkor kevésbé célzott hirdetéseket fognak megjelenni.',
+			]
+		]
+	]
+];
