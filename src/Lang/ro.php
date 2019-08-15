be664247ea8d41a7f22c55490680886d2e0be8c8
attreid <attreid@gmail.com>
2019-08-15 13:04:00 +0200

    ro

---
+++ src/Lang/ro.php
@@ -0,0 +1,39 @@
+﻿<?php
+
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Folosim cookie-uri pentru a putea adapta conținutul și reclamele fiecărui utilizator, pentru a asigura funcțiile de social media și pentru a analiza setările web-site-ului nostru. De asemenea, împărtășim informații despre ceea ce faceți pe acest site cu partenerii noștrii de social media, cu cei ce se ocupă de publicitate și prelucrarea analizelor, și facem acest lucru în concordanță cu documentul de Politica prelucrării datelor cu caracter personal. În secțiunea „Setări cookie”, vă puteți edita preferințele. Prin utilizarea acestui web-site, sunteți de acord cu utilizarea cookie-urilor.',
+			'link' => 'Mai multe informații',
+			'settings' => 'Setări Cookie',
+			'privacySettings' => 'Setări de confidențialitate',
+			'allowAll' => 'Permite totul',
+			'save' => 'Salvează setări',
+			'allowWithoutCommercial' => 'Permite fără reclame direcționate',
+			'title' => 'Centrul de preferințe pentru confidențialitate',
+			'alwaysActive' => 'Mereu activ',
+			'active' => 'Activ',
+			'inactive' => 'inactiv',
+			'privacy' => [
+				'title' => 'Intimitatea dvs.',
+				'content' => 'Paginile web pe care le vizitați pot citi sau salva informații din browser-ul dvs., de cele mai multe ori sub forma fișierelor cookie. Aceste informații pot face referire la dvs., la preferințele dvs. sau ale echipamentelor pe care le folosiți și sunt utilizate de regulă pentru a asigura performanța optimă a site-ului. Este vorba despre informații, care de obicei nu permit identificarea dvs. directă, dar care pot ajuta site-ul să se adapteze nevoilor și intereselor dvs.<br/><br/>Pentru că vă respectăm dreptul la intimitate, vă oferim dreptul să blocați anumite tipuri de fișiere cookie. Apăsați click pe fiecare categorie pentru a citi mai multe informații și pentru a modifica setările de bază. Daca optați pentru blocarea anumitor tipuri de fișiere cookie vă informăm că este posibil ca site-ul să nu funcționeze optim și să nu vă poate pune la dispoziție anumite servicii.',
+			],
+			'needed' => [
+				'title' => 'Fișiere cookie absolut necesare',
+				'content' => 'Aceste fișiere cookie sunt necesare pentru funcționarea site-ului iar sistemul nostru nu permite dezactivarea lor. De obicei sunt setate astfel încât să reacționeze numai la acțiunile dvs., deci la solicitarea unor servicii, cum ar fi setarea preferințelor de confidențialitate, conectarea sau completarea unui formular. Fără aceste fișiere cookie nu v-am fi putut pune la dispoziție anumite părți ale site-ului sau să vă furnizăm anumite servicii solicitate.',
+			],
+			'analytics' => [
+				'title' => 'Fișiere cookie analitice',
+				'content' => 'Aceste fișiere cookie ne permit să urmărim numărul vizitelor și de unde provin utilizatorii site-ului nostru, astfel încât să putem măsura și îmbunătăți performanța site-ului. Mulțumită acestor fișiere știm care pagini web sunt cele mai preferate și cele mai puțin preferate și în ce mod vizitatorii navighează prin site.<br/><br/>Toate informațiile pe care aceste fișiere le adună sunt agregate, fiind deci anonime. Dacă veți bloca aceste fișiere cookie nu vom ști când ne vizitați pagina web și deci nu vom putea urmări cum merge site-ul.',
+			],
+			'functional' => [
+				'title' => 'Fișiere cookie funcționale',
+				'content' => 'Aceste tipuri de fișiere permit ca site-ul să se adapteze mai bine nevoilor dvs. și să vă poată oferi mai multe funcții. Printre altele, ele folosesc la salvarea alegerilor dvs., limba sau locația preferată. Acest tip de fișiere cookie sunt setate fie de noi, fie de terțe părți, ale căror servicii sunt prezentate la noi pe web-site.<br/><br/>Dacă nu veți permite aceste fișiere cookie, este posibil ca unele sau chiar toate serviciile oferite de terțe părți să nu funcționeze corect.',
+			],
+			'commercial' => [
+				'title' => 'Fișiere cookie publicitare',
+				'content' => 'Aceste fișiere cookie sunt setate fie de noi sau de partenerii noștrii agenții de publicitate. Aceste companii le pot folosi pentru a crea profilul intereselor dvs., în baza căruia vă pot oferi reclame relevante pe paginile web-site-urilor lor.<br/><br/>Ele nu salvează datele personale în mod direct, însă lucrează cu browsere unice și echipamente speciale de identificare, prin intermediul cărora vă conectați la internet. Dacă veți bloca aceste tipuri de fișiere cookie, veți vedea anunțuri mai puțin direcționate.',
+			]
+		]
+	]
+];
