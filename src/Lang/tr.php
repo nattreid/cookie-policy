c4a886113220ca573560ac7a2bf75b21ef765f00
attreid <attreid@gmail.com>
2019-10-30 10:41:50 +0100

    tr

---
+++ src/Lang/tr.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Çerez dosyalarını içerik ve reklamları adapte etmek, sosyal medya işlevi sağlamak ve bireysel kullanıcıların websitemize ziyaretlerini analiz etmek amacıyla kullanıyoruz.  Kişisel verileri işleme ilkelerine uygun olarak sosyal medya, reklamcılık ve analiz süreçleri için websitemizdeki çalışmalarınız hakkındaki bilgileri ortaklarımızla paylaşıyoruz. Bu kısımda „Çerez dosyalarının ayarlanması, tercihlerinizi düzenleyebilirsiniz. Bunu kullanarak çerez dosyalarını kabul etmiş bulunursunuz.',
+			'link' => 'Daha fazla bilgi',
+			'settings' => 'Çerez ayarları',
+			'privacySettings' => 'Gizlilik ayarları',
+			'allowAll' => 'Hepsine izin ver',
+			'save' => 'Ayarları kaydet',
+			'allowWithoutCommercial' => 'Hedeflenmiş reklam olmadan izin ver',
+			'title' => 'Gizlilik koruma tercikleri merkezi',
+			'alwaysActive' => 'Her zaman aktif',
+			'active' => 'Aktif',
+			'inactive' => 'Aktif değil',
+			'privacy' => [
+				'title' => 'Gizliliğiniz',
+				'content' => 'Ziyeret ettiğiniz websiteleri, genellikle çerez dosyaları kullanarak, tarayıcınızdan bilgilerinizi kaydedebilir veya yükleyebilir. Sizi, tercihlerinizi ve ekipmanınızı ilgilendirebilecek olan bu bilgiler, websitelerinin optimal çalışmasını güven altına almak içindir. Bu bilgiler genellikle kimliğinizi doğrudan kontrol etmeye yaramaz, ancak websitelerini gereksinimlerinize ve ilgilerinize göre adapte etmeye yardımcı olurlar.<br/><br/>Gizliliğiniz üzerinde söz hakkı sahibi olmanızı önemsiyor ve bazı size bazı çerez dosyalarını engelleme imkanını sunuyoruz. İlgili kategoriye tıkladıktan sonra, başlangıç ayarlarınızı düzenlemeyle ilgili detaylı bilgiye ulaşacak ve düzenleme imkanlarınızı göreceksiniz. Belirli türlerdeki çerez dosyalarını engellerseniz, websitesi optimal düzeyde çalışır ve bazı servislerin kullanımına gerek kalmaz.',
+			],
+			'needed' => [
+				'title' => 'Kesinlikle gerekli olan çerez dosyaları',
+				'content' => 'Bu çerez dosyaları websitemizin çalışması için gereklidir ve sistemlerimizde bunları devre dışı bırakmamız olanaksızdır. Bunlar genellikle eylemlerinize, yani hizmet taleplerine göre, cevap verecek şekilde ayarlanmıştır, örn. gizlilik koruma tercihlerinizi belirleme, kayıt olma veya form doldurma. Web sayflarının bazı kısımlarını erişilebilir kılamıyor veya bazı gerekli hizmetleri bu çerez dosyaları olmadan sunamıyoruz.',
+			],
+			'analytics' => [
+				'title' => 'Analitik çerez dosyaları',
+				'content' => 'Bu çerez dosyaları ziyaret sayılarını ve ziyaretçilerin nereden geldiğini görebilmemize ve websitemizi geliştirebilmemize olanak sağlıyorlar. Bunun sayesinde, en sevilen sayfalar hangileri, en az tercih edilenler hangileri ve ziyaretçilerin hareketleri nasıl öğrenebiliyoruz.<br/><br/>Çerez dosyalarının topladığı tüm bilgiler anonimdir. Eğer websitemizi ziyaret ederken bu dosyaları engellerseniz, sitemizin nasıl çalıştığını kontrol edemeyebiliriz.',
+			],
+			'functional' => [
+				'title' => 'İşlevsel çerez dosyaları',
+				'content' => 'Bu çerez dosyaları websitemizi size göre adapte edebilmemizi ve size daha fazla işlev sunabilmemizi sağlar. Ayrıca seçimlerinizi, tercih edilen dil ayarlarınızı, hedeflerinizi kaydetmek için bulunurlar. Websitemizde sunduğumuz çerez dosyaları bizim tarafımızdan ya da üçüncü partiler tarafından kurulur.<br/><br/>Bu durumda, bu işlevi onaylamazsanız, bir kısmı veya hiçbiri doğru bir şekilde çalışmayacaktır.',
+			],
+			'commercial' => [
+				'title' => 'Reklam çerez dosyaları',
+				'content' => 'Bu çerez dosyaları bizim tarafımızdan veya reklam ortaklarımız tarafından kurulur. Bu şirketler ilgi alanlarınızın profilini oluşturabilir ve bu sayede size alakalı reklamlar sunabilirler.<br/><br/>Kişisel bilgilerinizi direkt olarak kaydetmezler, ancak, sayesinde internete bağlandığınız benzersiz tarayıcı kimliğiniz ve ekipmanınız ile çalışırlar. Bu durumda, eğer bu çerez dosyalarını engellerseniz, daha az hedeflenmiş reklam görürsünüz.',
+			]
+		]
+	]
+];
