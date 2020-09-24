792564a22f394c2072bdc853bde5f888d061b713
attreid <attreid@gmail.com>
2020-09-24 10:46:58 +0200

    lang no

---
+++ src/Lang/no.php
@@ -0,0 +1,38 @@
+﻿<?php
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'tekst' => 'Vi bruker informasjonskapselfiler (cookie filer) til å tilpasse innholdet og annonseringen, tilby sosiale medier og analysere tilstedeværelsen på nettet vårt for individuelle brukere. Vi deler informasjon om arbeidet ditt på nettet med våre partnere for sosiale medier, annonsering og behandling av analyser i samsvar med prinsippene for behandling av personlig opplysningsdokumenter. I en del "Innstilling av informasjonskapselfiler (cookie filer) kan du stille inn dine preferanser. Ved å bruke den godtar du informasjonskapslerfilene (cookie filene).',
+			'link' => 'Mer informasjon',
+			'innstillinger' => 'Cookie innstilling',
+			'personvernInnstilling' => 'Innstilling av personvern',
+			'tillatAlt' => 'Tillat alt',
+			'lagre' => 'Lagre innstillingen',
+			'tillatelseUtenReklame' => 'Tillat uten målrettet annonsering',
+			'tittel' => 'Senter for personvern beskyttelse preferanser',
+			'alltidAktiv' => 'Alltid aktiv',
+			'aktiv' => 'Aktiv',
+			'inaktiv' => 'Ikke aktiv',
+			'personvern' => [
+				'tittel' => 'Ditt personvern',
+				'innhold' => 'Nett, som du besøker, kan lagre eller laste informasjon fra nettleseren din, vanligvis med informasjonskapselfiler (cookie filer). Denne informasjonen kan  angå deg, dine preferanser eller utstyret ditt, og den brukes til å sikre optimal arbeid på nettet. Det handler om informasjon, som vanligvis ikke muliggjør direkte kontroll av identiteten din, men som kan bidra til å tilpasse nettet til dine krav og interesser.<br/><br/>Vi respekterer at du har rett til ditt personvern, så vi gir deg muligheten til å forby noen informasjonskapselfiler (cookie filer). Etter at du klikker på en enkel kategori vil du se detaljert informasjon og du har mulighet til å justere den opprinnelige innstillingen. Hvis du forbyr noen typer informasjonskapsler (cookie filer), fungerer nettet optimalt og trenger ikke å bruke noen tjenester.',
+			],
+			'nødvendig' => [
+				'tittel' => 'Helt nødvendige informasjonskapsler (cookie filer)',
+				'innhold' => 'Disse informasjonskapselfilene (cookie filene) er nødvendige for å fungere på nettet, og det er ikke mulig å deaktivere dem i systemene våre. Det er vanligvis satt til å reagere i henhold til din handling, så i henhold til tjenesteforespørsler, for eksempel innstilling av personvern beskyttelse preferanser, registrering eller utfylling av skjema. Vi kunne ikke gjøre tilgjengelig noen nettdeler eller tilby noen nødvendige tjenester uten disse informasjonskapselfilene (cookie filene).',
+			],
+			'analytisk' => [
+				'tittel' => 'Analytiske informasjonskapselfiler (cookie filer)',
+				'innhold' => 'Disse informasjonskapsfilene (cookie filene) gjør det mulig for oss å overvåke antall besøk og hvor brukerne kommer fra for å måle og forbedre nettarbeid. Vi vet, takket være disse, hvilke sider som er mest populære, hvilke som er minst, og hvordan brukerne beveger seg på nettet.<br/><br/>All informasjon, som disse informasjonskapselfilene (cookie filene) samler inn er anonyme. Hvis du forbyr disse informasjonskapsfilene (cookie filene) når du besøker nettet vårt, vil vi ikke kunne sjekke hvordan nettet fungerer.',
+			],
+			'funksjonal' => [
+				'tittel' => 'Funksjonelle informasjonskapselfiler (cookie filer)',
+				'innhold' => 'Disse informasjonskapsfilene (cookie filene) gjør det mulig å tilpasse nettet for deg og tilby deg flere funksjoner. Det er også for å lagre valg, foretrukket språk eller destinasjon. informasjonskapselfiler (cookie filer) blir puttet av oss eller av den tredje delen, som vi presenterer på nettet vårt.<br/><br/>I tilfelle du ikke bekrefter denne funksjonen, vil noen eller ingen av dem ikke fungere riktig.',
+			],
+			'kommersiell' => [
+				'tittel' => 'Annonsering av informasjonskapselfiler (cookie filer)',
+				'innhold' => 'Disse informasjonskapselfilene (cookie filene) er angitt av oss eller våre partnerskapsannonsører. Disse selskapene kan lage en profil for dine interesser, og takket være dem tilbyr de deg relevant annonsering. <br/><br/>De lagrer ikke direkte personlige data, men jobber med unik nettleser identifisering og utstyr, takket være den du kobler til internett. I tilfelle du forbyr disse informasjonskapsfilene (cookie filene) , vil du se mindre målrettet annonsering.',
+			]
+		]
+	]
+];
