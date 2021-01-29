4f82e292293512cdbb154ebe272fefb1718a211c
attreid <attreid@gmail.com>
2021-01-29 14:04:16 +0100

    add lang sv

---
+++ src/Lang/sv.php
@@ -0,0 +1,38 @@
+<?php
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Vi använder cookies för att anpassa innehållet och annonseringen, erbjuda sociala medier och analysera vår online närvaro för enskilda användare. Vi delar information om ditt arbete online med våra partners för sociala medier, reklam och bearbetning av analys i enlighet med principerna för behandling av personuppgiftsdokument. I avsnittet "Ställa in cookies kan du ställa dina preferenser. Och genom att använda det accepterar du cookies..',
+			'link' => 'Mer information',
+			'settings' => 'Cookie inställning',
+			'privacySettings' => 'inställning av Integritet',
+			'allowAll' => 'Tillåt alla',
+			'save' => 'Spara inställning',
+			'allowWithoutCommercial' => 'Tillåtelse utan riktad reklam',
+			'title' => 'Centrum för integritets skyddspreferenser',
+			'alwaysActive' => 'Alltid aktiv',
+			'active' => 'Aktiv',
+			'inactive' => 'Inte aktiv',
+			'privacy' => [
+				'title' => 'Din integritet',
+				'content' => 'Webbplatser som du besöker kan lagra eller ladda information från din webbläsare, vanligtvis med cookies. Denna information kan beröra dig, dina preferenser eller din utrustning, och den används för att säkerställa optimalt arbete online. Det handlar om information som vanligtvis inte möjliggör direkt kontroll av din identitet, men som kan hjälpa dig att anpassa webben till dina krav och intressen. <br/> <br/> Vi respekterar att du har rätt till din integritet, så vi ger dig möjligheten att förbjuda alla cookiefiler. När du har klickat på den enskilda kategorin ser du detaljerad information och har möjligheten att justera den originala inställningen. Om du förbjuder vissa typer av cookies fungerar webben optimalt och du behöver inte använda några tjänster.', Necessary
+			],
+			'needed' => [
+				'title' => 'Helt nödvändiga  cookies',
+				'content' => 'Dessa cookiefiler krävs för att fungera online och det är inte möjligt att inaktivera dem i vårt system. Det är vanligtvis inställt på att svara enligt din åtgärd, så enligt tjänsteförfrågningar, t.ex. ställa in sekretes skyddspreferenser, registrera eller fylla i formulär. Utan dessa cookiefiler kunde vi inte göra några webbdelar tillgängliga eller tillhandahålla några nödvändiga tjänster..',
+			],
+			'analytics' => [
+				'title' => 'Analytiska cookiefiler',
+				'content' => 'Dessa cookiefiler tillåter oss att övervaka antalet besök och var användare kommer ifrån för att mäta och förbättra webbarbetet. Tack vare dessa vilka sidor vet vi even som är mest populära, vilka som är minst och hur användare flyttar online. <br/> <br/> All information som dessa cookies samlar in är anonym. Om du förbjuder dessa cookies filer när du besöker vår webbplats kan vi inte kontrollera hur webben fungerar.',
+			],
+			'functional' => [
+				'title' => 'Funktionella cookiefiler',
+				'content' => 'Med dessa cookiefiler kan du anpassa webben åt dig och erbjuda dig fler funktioner. Det är också för att spara val, önskat språk eller destination. Cookies ställs in av oss eller av den tredje sidan, som vi presenterar på vår webbplats. <br/> <br/> Om du inte bekräftar den här funktionen kommer vissa eller inga av dem att fungera korrekt...',
+			],
+			'commercial' => [
+				'title' => 'Annonsering av cookie filer',
+				'content' => 'Dessa cookiefiler tillhandahålls av oss eller våra partner annonsörer. Dessa företag kan skapa en profil för dina intressen, och tack vare den erbjuder de dig relevant annonsering. <br/> <br/> De lagrar inte direkt personuppgifter utan arbetar med unik bläddringsidentifiering och utrustning, tack vare den du anslut Internet. Om du förbjuder dessa cookiefiler kommer du att se mindre riktad reklam.',
+			]
+		]
+	]
+];
