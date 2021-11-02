f5345718fa60bbbfffe6066b543502b782b746b1
attreid <attreid@icloud.com>
2021-11-02 14:05:22 +0100

    add fr

---
+++ src/Lang/fr.php
@@ -0,0 +1,38 @@
+<?php
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Nous utilisons des cookies pour adapter le contenu et les publicités à des utilisateurs spécifiques, pour fournir des fonctionnalités de médias sociaux et pour analyser le trafic vers notre site Web. Nous partageons également des informations sur votre travail avec le site Web avec nos partenaires de médias sociaux, de publicité et d\'analyse, conformément à la politique de confidentialité. Vous pouvez ajuster vos préférences dans la section «Paramètres des cookies». En utilisant ce site, vous acceptez l\'utilisation des cookies.',
+			'link' => 'Plus d\'information',
+			'settings' => 'Paramètres Cookie',
+			'privacySettings' => 'Paramètres de confidentialité',
+			'allowAll' => 'Tout autoriser',
+			'save' => 'Enregistrer les paramètres',
+			'allowWithoutCommercial' => 'Autoriser sans publicités ciblées',
+			'title' => 'Centre de préférences de confidentialité',
+			'alwaysActive' => 'Toujours actif',
+			'active' => 'Actif',
+			'inactive' => 'Inactif',
+			'privacy' => [
+				'title' => 'Votre vie privée',
+				'content' => 'Les sites Web visités peuvent stocker ou récupérer des informations de votre navigateur, généralement sous la forme de cookies. Ces informations peuvent concerner vous, vos préférences ou votre appareil et sont utilisés en général pour établir le fonctionnement optimal du web. Il s\'agit d´informations qui ne permettent pas de vous identifier directement, mais peuvent aider le site à s´adapter à vos besoins et vos intérêts.<br/><br/>Parce que nous respectons votre droit à la vie privée nous vous offrons la possibilité de désactiver certains types de cookies. Après avoir cliqué sur les catégories individuelles, vous pouvez lire des informations plus détaillées et ajuster les paramètres par défaut. Si certains types de cookies sont bloqués, le site Web peut ne pas fonctionner de manière optimale et certains services peuvent ne pas être disponibles.',
+			],
+			'needed' => [
+				'title' => 'Cookies essentiels',
+				'content' => 'Ces fichiers cookies sont nécessaires au fonctionnement du web et il n\'est pas possible de les désactiver dans nos systèmes. Ils sont généralement configurés pour répondre uniquement à vos actions,telles que les demandes de service comme la modification des préférences de confidentialité, la connexion ou le remplissage d\'un formulaire. Sans ces cookies, nous ne serions pas en mesure de vous permettre d\'accéder à certaines parties du site ou de fournir certains des services requis.',
+			],
+			'analytics' => [
+				'title' => 'Cookies analytiques',
+				'content' => 'Ces Cookies nous permetent de suivre le nombre des visites de notre web et suivre d´où les utilisateurs viennent sur notre site Web afin que nous puissions mesurer et améliorer les performances du site Web.En conséquence, nous savons quels sites sont les plus populaires et les moins populaires et comment les visiteurs naviguent sur le site.<br/><br/>Toutes les informations accumulées par ces cookies sont agrégées et donc anonymes. Si vous désactivez ces cookies, nous ne saurons pas quand vous visitez notre site et nous ne pourrons pas surveiller les performances du site.',
+			],
+			'functional' => [
+				'title' => 'Cookies fonctionnels',
+				'content' => 'Ces cookies permettent au site de s\'adapter au mieux à vous et de vous offrir plus de fonctionnalités. Entre autres choses, ils sont utilisés pour stocker vos sélections, votre préférence de langue ou votre emplacement. Ce type de cookie est installé par nous ou par des tiers dont les services nous présentons sur notre site. <br/> <br/> Si vous n\'activez pas ces cookies, certains ou aucun de ces services peuvent ne pas fonctionner correctement.',
+			],
+			'commercial' => [
+				'title' => 'Cookies publicitaires',
+				'content' => 'Ces cookies sont installés par nous ou nos partenaires annonceurs. Ces sociétés peuvent vous aider à profiler vos centres d\'intérêt et vous proposer des publicités similaires sur d\'autres sites. <br/> <br/> Elles ne stockent pas d\'informations personnelles directement, mais fonctionnent avec l\'identification unique du navigateur et de l\'appareil auxquels vous vous connectez à l´Internet. Si vous désactivez ces cookies, vous verrez des publicités moins ciblées.',
+			]
+		]
+	]
+];
