26fa3eae02119e57a2692077051d8cbb3fd5c0d0
attreid <attreid@icloud.com>
2022-03-17 09:37:49 +0100

    da lang

---
+++ src/Lang/da.php
@@ -0,0 +1,38 @@
+<?php
+return [
+	'nattreid' => [
+		'cookiePolicy' => [
+			'text' => 'Vi bruger cookiefiler til at tilpasse indholdet og annonceringen, levere sociale mediefunktioner og analysere deltagelse på vores web for individuelle brugere. Vi deler information om dit arbejde på nettet med vores partnere til sociale medier, annoncering og behandling af analyser i overensstemmelse med principper for behandling af persondatadokumenter. I en del "Indstilling af cookiefiler kan du indstille dine præferencer. Ved at bruge det accepterer du cookiefiler.',
+			'link' => 'Mere information',
+			'settings' => 'Cookie indstilling',
+			'privacySettings' => 'Indstilling af personlige oplysninger',
+			'allowAll' => 'Tillad alle',
+			'save' => 'Gem indstilling',
+			'allowWithoutCommercial' => 'Tilladelse uden målrettet reklame',
+			'title' => 'Center af personlige oplysninger',
+			'alwaysActive' => 'Altid aktiv',
+			'active' => 'Aktiv',
+			'inactive' => 'Ikke aktiv',
+			'privacy' => [
+				'title' => 'Dine personlige oplysninger',
+				'content' => 'Webs, som du besøger, kan gemme eller indlæse information fra din browser, normalt ved hjælp af cookiefiler. Disse oplysninger kan vedrøre dig, dine præferencer eller dit udstyr, og de bruges til at sikre optimalt web-arbejde. Det handler om information, som normalt ikke muliggør direkte kontrol af din identitet, men som kan hjælpe med at tilpasse nettet til dine krav og interesser.<br/><br/>Vi respekterer, at du har ret til dit privatliv, så vi tilbyde dig muligheden for at forbyde nogle cookiefiler. Efter klik på den enkelte kategori vil du se detaljeret information og mulighed for at justere den oprindelige indstilling. Hvis du forbyder nogle typer af cookiefiler, fungerer nettet optimalt og behøver ikke at bruge nogle tjenester.',
+			],
+			'needed' => [
+				'title' => 'Fuldstændig nødvendige cookiefiler',
+				'content' => 'Disse cookiefiler er nødvendige for at fungere på nettet, og det er ikke muligt at deaktivere dem i vores systemer. Det er normalt indstillet til at reagere i henhold til din handling, så ifølge serviceanmodninger, f.eks. indstilling af præferencer for beskyttelse af personlige oplysninger, registrering eller udfyldning af formular. Vi kunne ikke gøre nogle webdele tilgængelige eller levere nogle nødvendige tjenester uden disse cookiefiler.',
+			],
+			'analytics' => [
+				'title' => 'Analytiske cookiefiler',
+				'content' => 'Disse cookiefiler gør det muligt for os at overvåge antallet af besøg, og hvor brugerne kommer fra for at måle og forbedre web-arbejdet. Takket være det ved vi, hvilke sider der er mest foretrukne, hvilke der er færre, og hvordan brugerne bevæger sig på nettet.<br/><br/>Alle oplysninger, som denne cookie-filer indsamler, er anonyme. Hvis du forbyder disse cookie-filer, mens du besøger vores web, kunne vi ikke kontrollere, hvordan internettet fungerer.',
+			],
+			'functional' => [
+				'title' => 'Funktions-cookiefiler',
+				'content' => 'Disse cookiefiler gør det muligt at tilpasse nettet til dig og tilbyde dig flere funktioner. Det er også til at gemme dine valg, foretrukne sprog eller destination. Cookiefiler sættes af os eller af den tredje side, som vi præsenterer på vores web.<br/><br/>Hvis du ikke bekræfter denne funktion, vil nogle eller ingen af dem ikke fungere korrekt.',
+			],
+			'commercial' => [
+				'title' => 'Annonce-cookiefiler',
+				'content' => 'Disse cookiefiler er sat af os eller vores partnerskabsannoncører. Disse virksomheder kan oprette en profil af dine interesser, og takket være den tilbyder de dig relevant reklame.<br/><br/>De gemmer ikke direkte personlige data, men arbejder med unik browse-identifikation og udstyr, takket være det opretter du forbindelse til internettet. I tilfælde af at du forbyder denne cookiefiler, vil du se mindre målrettet annoncering.',
+			]
+		]
+	]
+];
